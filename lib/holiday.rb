90f8be3262d766ae828642420e3a07dadf9a8bc3
JC <bonassin@gmail.com>
2020-12-24 23:38:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,14 @@ end
 
 
 
-def all_holidays_with_bbq(holiday_hash)
+  def all_holidays_with_bbq(holiday_hash)
+    holiday_hash.map do |season, holidays|
+      holidays.map do |holiday, supplies|
+        holiday if supplies.include?("BBQ")
+      end
+    end.flatten.compact
+  end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-end
+
